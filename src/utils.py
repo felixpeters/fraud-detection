98d551546af20716d80a272e2d14d16342371bdb
Felix Peters <felix@felixpeters.me>
2023-06-13 16:13:29 +0200

    load yaml config files

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,24 @@
+from pathlib import Path
+from typing import Dict
+
+import yaml
+
+
 def add(a: int, b: int) -> int:
     return a + b
+
+
+def load_config(path: Path) -> Dict:
+    """Load config from yaml file.
+
+    Args:
+        path (Path): Path to YAML file.
+
+    Returns:
+        Dict: Dictionary containing configuration.
+    """
+    with open(path) as f:
+        config = yaml.safe_load(f)
+    if not isinstance(config, dict):
+        return None
+    return config
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,26 @@
-from src.utils import add
+import yaml
+
+from src.utils import add, load_config
 
 
 def test_add():
     assert add(1, 2) == 3
     assert add(0, 0) == 0
     assert add(-1, 1) == 0
+
+
+def test_load_config(tmp_path):
+    # Create a temporary YAML file with test data
+    config_path = tmp_path / "test_config.yaml"
+    expected_config = {"key1": "value1", "key2": "value2"}
+    with open(config_path, "w") as f:
+        yaml.dump(expected_config, f)
+
+    # Test loading a valid YAML file
+    assert load_config(config_path) == expected_config
+
+    # Test loading an invalid YAML file
+    config_path = tmp_path / "test_config_invalid.yaml"
+    with open(config_path, "w") as f:
+        f.write("invalid_yaml")
+    assert load_config(config_path) is None
